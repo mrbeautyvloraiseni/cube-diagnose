422ebae1f14133f90175c3eb06bd6b260f1708f8
MR Beauty <mrbeautyvlora@hotmail.com>
2026-09-04 18:39:03 +0200

    Fix Cube Diagnose JVM Toolchain 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 android {
     namespace = "ch.mrbeauty.cubediagnose"
     compileSdk = 35
@@ -13,6 +17,15 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     buildTypes {
