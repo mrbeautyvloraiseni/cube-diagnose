ce2200484d76637be17d5f6b104db7602bfef6e6
MR Beauty <mrbeautyvlora@hotmail.com>
2026-09-04 18:23:41 +0200

    Fix Cube Diagnose JVM 17

---
+++ app/app_build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "ch.mrbeauty.cubediagnose"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "ch.mrbeauty.cubediagnose"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
